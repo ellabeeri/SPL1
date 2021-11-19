614dbc3ea990663a32742c21c1809f579aec68ac
ellabeeri <94682619+ellabeeri@users.noreply.github.com>
2021-11-19 14:01:40 +0200

    Update Trainer.cpp

--- Trainer.cpp
+++ Trainer.cpp
@@ -1,14 +1,10 @@
 //
-// Created by liorlevy on 17/11/2021.
-//
-
-//#include "Trainer.h"
-//
 // Created by ellabeeri on 10/11/2021.
 //
 
 #include "Trainer.h"
 #include "Studio.h"
+#include "Customer.h"
 
 int Trainer::getCapacity() const {
     return capacity;
@@ -16,11 +12,24 @@ int Trainer::getCapacity() const {
 
 void Trainer::addCustomer(Customer *customer) {
     customersList.push_back(customer);
+    capacity--;
+    //std::vector<int> workOutID = customer->order(Studio().getWorkoutOptions());
 
 }
 
 void Trainer::removeCustomer(int id) {
     customersList.pop_back();
+    capacity++;
+    if (customersList.empty())
+        closeTrainer();
+    for (int i = 0; i < orderList.size(); ++i) {
+        if (orderList[i].first == id) {
+            salary=salary-orderList[i].second.getPrice();
+            orderList.erase(orderList.begin() + i);
+        }
+
+
+    }
 }
 
 Customer *Trainer::getCustomer(int id) {
@@ -39,34 +48,34 @@ std::vector<Customer *> &Trainer::getCustomers() {
 std::vector<OrderPair> &Trainer::getOrders() {
     return orderList;
 }
-//std::vector<OrderPair> output;
-//  for (int i = 0; i < getCustomers().size(); i++)//run on all customers
-// {
-//  for (int j = 0; j < getCustomers()[i]->order(Studio().getWorkoutOptions()).size(); j++) {
-//    int workoutID = getCustomers()[i]->order(Studio().getWorkoutOptions())[j];
-
-//output.push_back( );
-//  OrderPair a = new OrderPair <>;
 
 
+void
+Trainer::order(const int customer_id, const std::vector<int> workout_ids, const std::vector<Workout> &workout_options) {
 
-void Trainer::order(const int customer_id, const std::vector<int> workout_ids, const std::vector<Workout> &workout_options) {
+    for (int i = 0; i < workout_ids.size(); i++) {
+        for (int j = 0; j < workout_options.size(); j++) {
+            if (workout_options[j].getId() == workout_ids[i]) {
+                OrderPair pairIdWorkout = std::make_pair(customer_id, workout_options[j]);
+                salary=salary+pairIdWorkout.second.getPrice();
+                orderList.push_back(pairIdWorkout);
+            }
+        }
+    }
 
-    getCustomer(customer_id)->order(workout_options);
 }
 
 void Trainer::openTrainer() {
     open = true;
-
-
 }
 
 void Trainer::closeTrainer() {
-
+    open = false;
+    capacity--;
 }
 
 int Trainer::getSalary() {
-    return;
+    return salary;
 }
 
 bool Trainer::isOpen() {
@@ -74,16 +83,11 @@ bool Trainer::isOpen() {
     return open;
 }
 
-Trainer::Trainer(int t_capacity) {
-
-}
-
-Workout workoutFromID(int id) {
-    for (int k = 0; k < Studio().getWorkoutOptions().size(); k++) {
-        //running on workout option and compare it to customers workout number.
-
-        if (Studio().getWorkoutOptions()[k].getId() == id)
-            return Studio().getWorkoutOptions()[k];
-    }
+Trainer::Trainer(int t_capacity) : capacity(t_capacity), limitCapacity(t_capacity), open(false) {
+    std::vector<Customer *> c;
+    customersList = c;
+    std::vector<OrderPair> a;
+    orderList = a;
+    salary = 0;
 
 }
